faed67cb147b6f330f14d0203aab300e313e2c9b
aavolo <aavolo@gmail.com>
2019-09-09 14:00:10 -0700

    Adding a HelloWorld file. Delete later.

---
+++ fall19/assignment01/HelloWorld.java
@@ -0,0 +1,10 @@
+
+
+public class HelloWorld {
+		public static int i = 1;
+		public static int j = ++i;
+	public static void main(String args[]) {
+
+		System.out.println("i = " + i + " and j = " + j);
+	} 
+}
