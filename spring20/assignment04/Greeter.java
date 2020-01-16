83cf191f05832bb0a431ed03248fe95e6c1c5d14
aavolo <aavolo@gmail.com>
2020-01-16 10:41:56 -0800

    adding greeter to assignment04, delete later

---
+++ spring20/assignment04/Greeter.java
@@ -0,0 +1,22 @@
+//This is a single line comment.
+
+/* This is a
+   multi line
+   comment
+*/
+
+ /**
+  * This is a doc comment.
+  */
+
+/**
+ * The Greeter program prints "Hello, world!" to the console.
+ */
+
+
+public class Greeter {
+	public static void  main ( String[] args) {
+        System.out.println("hey there");
+	}
+}
+
